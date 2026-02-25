de7fc36fc76f5fefb87601106b465613d0cce14e
Nazarii Shvets <n.shvets@goldenratio.exchange>
2026-02-25 18:57:35 +0200

    fix: hero layout for other locales

--- apps/web/src/app/[locale]/Hero.tsx
+++ apps/web/src/app/[locale]/Hero.tsx
@@ -20,7 +20,7 @@ export async function HeroSection() {
     >
       <div className="relative flex flex-col flex-grow sm:justify-between lg:justify-center lg:items-center gap-8 mx-auto pt-[160px] md:pt-[116px] lg:pt-[80px] pb-20 lg:pb-[160px] w-full max-w-[1080px]">
         <div className="flex flex-col px-4 md:px-8 lg:px-8 xl:px-0">
-          <h1 className="mb-4 md:mb-6 font-unbounded font-bold text-[48px] text-neutral md:text-[64px] leading-[56px] md:leading-[72px] md:tracking-[0.32px]">
+          <h1 className="mb-4 md:mb-6 font-unbounded font-bold text-[36px] text-neutral md:text-[64px] leading-[40px] md:leading-[72px] md:tracking-[0.32px]">
             {t.rich("hero.title", {
               highlight: (chunks) => (
                 <span className="text-gold-500">{chunks}</span>
@@ -33,7 +33,7 @@ export async function HeroSection() {
             {t("hero.subtitleLine2")}
           </h2>
 
-          <div className="min-[425px]:flex min-[425px]:justify-center min-[425px]:items-center gap-3 grid grid-cols-2">
+          <div className="min-[425px]:flex min-[425px]:justify-center min-[425px]:items-center gap-3 grid grid-cols-1 md:grid-cols-2">
             <ButtonRoot
               className="min-[425px]:min-w-[186px]"
               asChild
